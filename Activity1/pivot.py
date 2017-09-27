2b30fb8e572ee9e8dbcadb470661342b8c91d8e3
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-09-27 19:22:42 +0200

    Añadida lista en las columnas donde falla

--- Activity1/pivot.py
+++ Activity1/pivot.py
@@ -4,6 +4,14 @@
 """
 
 import csv
+def unique_list(l):
+    x = []
+    for a in l:
+        if a not in x:
+            x.append(a)
+    return x
+
+
 
 
 f = open("Data\dengue_features_train.csv", 'rt')
@@ -11,7 +19,7 @@ states = []
 est = ""
 
 try:
-    g = open("2009pivot.csv", 'wt')
+    g = open("Data\ 2009pivot.csv", 'wt')
     writer = csv.writer(g)
     writer.writerow(('city', 'year', 'weekofyear', 'week_start_date', 'ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw',
                      'precipitation_amt_mm', 'reanalysis_air_temp_k', 'reanalysis_avg_temp_k',
@@ -22,17 +30,25 @@ try:
                      'station_precip_mm'))
     reader = csv.reader(f)
     data = None
+
+    fallos=[]
     for row in reader:
+
         newest = row[0]
         year = row[1]
         if 'iq' == newest:
             if int(year) >= 2000 and int(year) <= 2003:
+
                 print row
                 del row[0]
                 for i in range(len(row)-1):
                     if (row[i] == ""):
-                        row[i] = 0
+                        row[i]=0
+                        fallos.append(i)
                         print "atenció"
                 writer.writerow(row)
+
+
+    fallos=sorted(unique_list(fallos))#lista de columnas donde falla y hay un 0
 finally:
     f.close()
