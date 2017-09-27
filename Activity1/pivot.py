41ebecb38dfd4a22519fa944d47a2c1cf94a8b44
Alvaro <Alvaro.AngelMoreno@alu.uclm.es>
2017-09-27 18:09:31 +0200

    Crea el pivot.csv

--- Activity1/pivot.py
+++ Activity1/pivot.py
@@ -10,6 +10,15 @@ f = open("Data\dengue_features_train.csv", 'rt')
 states = []
 est = ""
 try:
+    g = open("2009pivot.csv", 'wt')
+    writer = csv.writer(g)
+    writer.writerow(('city', 'year', 'weekofyear', 'week_start_date', 'ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw',
+                     'precipitation_amt_mm', 'reanalysis_air_temp_k', 'reanalysis_avg_temp_k',
+                     'reanalysis_dew_point_temp_k', 'reanalysis_max_air_temp_k', 'reanalysis_min_air_temp_k',
+                     'reanalysis_precip_amt_kg_per_m2', 'reanalysis_relative_humidity_percent',
+                     'reanalysis_sat_precip_amt_mm', 'reanalysis_specific_humidity_g_per_kg', 'reanalysis_tdtr_k',
+                     'station_avg_temp_c', 'station_diur_temp_rng_c', 'station_max_temp_c', 'station_min_temp_c',
+                     'station_precip_mm'))
     reader = csv.reader(f)
     data = None
     for row in reader:
@@ -18,23 +27,8 @@ try:
         if 'iq' == newest:
             if int(year) >= 2000 and int(year) <= 2003:
                 print row
-                if row[3].rfind("  ") == 2:
-                    data.append(row[4])
-        else:
-            if data != None:
-                states.append(data)
-            data = []
-            data.append(newest)
-            est = newest
-finally:
-    f.close()
-
-# write the results in a csv file
-f = open("2009pivot.csv",'wt')
-try:
-    writer = csv.writer(f)
-    writer.writerow(('city','year','weekofyear','week_start_date','ndvi_ne','ndvi_nw','ndvi_se','ndvi_sw','precipitation_amt_mm','reanalysis_air_temp_k','reanalysis_avg_temp_k','reanalysis_dew_point_temp_k','reanalysis_max_air_temp_k','reanalysis_min_air_temp_k','reanalysis_precip_amt_kg_per_m2','reanalysis_relative_humidity_percent','reanalysis_sat_precip_amt_mm','reanalysis_specific_humidity_g_per_kg','reanalysis_tdtr_k','station_avg_temp_c','station_diur_temp_rng_c','station_max_temp_c','station_min_temp_c','station_precip_mm'))
-    for i in range(1,len(states)):
-        writer.writerow((states[i]))
+                #for i in range(1,len(row)):
+                #data.append(row)
+                writer.writerow(row)
 finally:
     f.close()
