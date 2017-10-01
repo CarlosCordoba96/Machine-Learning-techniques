57d9fc80f4c1af344d606daa25df935e8a3bad33
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-10-01 20:15:56 +0200

    Organising the folders

---
+++ Activity1/ewqwew.py
@@ -0,0 +1,53 @@
+import codecs
+from numpy import corrcoef, transpose, arange
+from pylab import pcolor, show, colorbar, xticks, yticks
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 0. Load Data
+f = codecs.open("D:\Users\Carlos\workspace\Machine-Learning-techniques\Activity1\Data\ 2009pivot.csv", "r", "utf-8")
+states = []
+count = 0
+i=0;
+for line in f:
+    if count > 0:
+        # remove double quotes
+        row = line.replace ('"', '').split(",")
+        row.pop(0)
+        if row != []:
+            while i<=len(row):
+                row[i] = float(row[i])
+            states.append(map(float, row))
+    count += 1
+
+
+
+
+# plotting the correlation matrix
+#http://glowingpython.blogspot.com.es/2012/10/visualizing-correlation-matrices.html
+R = corrcoef(transpose(states))
+pcolor(R)
+colorbar()
+yticks(arange(0,17),range(0,17))
+xticks(arange(0,17),range(0,17))
+show()
+
+
+# http://stanford.edu/~mwaskom/software/seaborn/examples/many_pairwise_correlations.html
+# Generate a mask for the upper triangle
+sns.set(style="white")
+mask = np.zeros_like(R, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(200, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(R, mask=mask, cmap=cmap, vmax=.8,
+            square=True, xticklabels=2, yticklabels=2,
+            linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
