05f46ab525866d0940ce55a9eeb3373a709deec3
alvaroamoreno <alvaro.angel@hotmail.es>
2017-10-11 21:08:35 +0200

    Add files via upload

--- Milestone2/cluster_hierarchical.py
+++ Milestone2/cluster_hierarchical.py
@@ -18,10 +18,16 @@ import sklearn.neighbors
 import loaddata
 cases = loaddata.load_data_usa("Data/iq_2000_2003_pivot.csv")
  
- 
 ### 2. Normalization of the data
 min_max_scaler = preprocessing.MinMaxScaler()
 norm_cases = min_max_scaler.fit_transform(cases)
+
+#0.5. Tools for PCA
+from sklearn.decomposition import PCA
+estimator = PCA (n_components = 2)
+X_pca = estimator.fit_transform(norm_cases)
+
+plt.plot(X_pca[:,0], X_pca[:,1],'x')
 
 ### 3. Compute the similarity matrix
 dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean') 
@@ -30,7 +36,7 @@ avSim = numpy.average(matsim)
 print "%s\t%6.2f" % ('Average Distance', avSim)
 
 ### 4. Building the Dendrogram	
-cut = 6
+cut = 7
 clusters = cluster.hierarchy.linkage(matsim, method = 'complete')
 cluster.hierarchy.dendrogram(clusters, color_threshold = cut)
 plt.show()
@@ -52,4 +58,15 @@ for c in range(1,n_clusters_+1):
             s = "%s,%s" % (s,numpy.mean(column))
     print s
 
-# Sacar numero de elementos de cada cluster
+# PCA de clúster
+colors = numpy.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+colors = numpy.hstack([colors] * 20)
+numbers = numpy.arange(len(X_pca))
+fig, ax = plt.subplots()
+for i in range(len(X_pca)):
+    plt.text(X_pca[i][0], X_pca[i][1], numbers[i], color=colors[labels[i]]) 
+plt.xlim(-1, 1.5)
+plt.ylim(-1, 1.5)
+ax.grid(True)
+fig.tight_layout()
+plt.show()
