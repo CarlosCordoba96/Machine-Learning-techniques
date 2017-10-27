b879c53cb455978107348241f96a55b7ef396d97
alvaroamoreno <alvaro.angel@hotmail.es>
2017-10-27 16:21:55 +0200

    Comments

--- Milestone1/02_correlation.py
+++ Milestone1/02_correlation.py
@@ -10,7 +10,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# 0. Load Data
+# Load data 
 f = codecs.open("Data\ iq_2000_2003_pivot.csv", "r", "utf-8")
 states = []
 count = 0
@@ -23,8 +23,7 @@ for line in f:
 			states.append(map(float, row))#Using float
 	count += 1
 
-# plotting the correlation matrix
-#http://glowingpython.blogspot.com.es/2012/10/visualizing-correlation-matrices.html
+# Using as much range as the number of attributes that we have, prints the Correlation Matrix
 R = corrcoef(transpose(states))
 pcolor(R)
 colorbar()
@@ -32,20 +31,12 @@ yticks(arange(0,21),range(0,21))
 xticks(arange(0,21),range(0,21))
 show()
 
-
-# http://stanford.edu/~mwaskom/software/seaborn/examples/many_pairwise_correlations.html
-# Generate a mask for the upper triangle
+# Generates the triangle of correlation with the same elements, and prints it
 sns.set(style="white")
 mask = np.zeros_like(R, dtype=np.bool)
 mask[np.triu_indices_from(mask)] = True
-
-# Set up the matplotlib figure
 f, ax = plt.subplots(figsize=(11, 9))
-
-# Generate a custom diverging colormap
 cmap = sns.diverging_palette(200, 10, as_cmap=True)
-
-# Draw the heatmap with the mask and correct aspect ratio
 sns.heatmap(R, mask=mask, cmap=cmap, vmax=.8,
             square=True, xticklabels=2, yticklabels=2,
             linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
