0efd05bc2bc1e15ecbe4428488cdca66e554c440
alvaroamoreno <alvaro.angel@hotmail.es>
2017-10-27 16:47:01 +0200

    Commented

--- Milestone3/loaddata.py
+++ Milestone3/loaddata.py
@@ -7,18 +7,13 @@ Created on Mon Mar 14 12:05:59 2016
 import codecs
 def load_data():
 #We retake the code of a previour version because we have some problems with our code
-### 1. Load the data asigned
+#Defines the method that takes our data set and saves it into a variable to work with it
     try:
         f = codecs.open("Data/dengue_features_train.csv", "r", "utf-8")
         cases = []
         count = 0
         for line in f:
-            if count > 0:
-                # Insert a 0 in unfilled fields
-                while ",," in line:
-                    line = line.replace(',,', ',0,')
-                line = line.replace(',\n', ',0')
-                
+            if count > 0:             
                 row = line.split(",")
                 if row[0] == "iq":
                     if int(row[1])>=2000 and int(row[1])<=2003:
